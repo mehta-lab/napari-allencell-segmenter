29649d9b59ef1b2ed018061cf771303a8106acf2
camfoltz <camfoltz2@gmail.com>
2022-02-07 12:11:26 -0800

    add reader

--- napari_allencell_segmenter/_dock_widget.py
+++ napari_allencell_segmenter/_dock_widget.py
@@ -2,8 +2,12 @@
 import napari
 
 from napari_allencell_segmenter.core.application import Application
-from napari_plugin_engine import napari_hook_implementation
+# from napari_plugin_engine import napari_hook_implementation
 from qtpy.QtWidgets import QWidget, QVBoxLayout, QSizePolicy
+import dask.array as da
+from waveorder.io import WaveorderReader
+import zarr
+
 
 """
 The class name here gets converted to title case and gets displayed as both the title 
@@ -30,6 +34,43 @@ class BatchProcessingWidget(QWidget):
         self._application.router.batch_processing()  # Initialize first screen
 
 
-@napari_hook_implementation
+def ome_zarr_reader(path):
+    reader = WaveorderReader(path)
+    results = list()
+
+    zs = zarr.open(path, 'r')
+    names = []
+
+    dict_ = zs.attrs.asdict()
+    wells = dict_['plate']['wells']
+    for well in wells:
+        path = well['path']
+        well_dict = zs[path].attrs.asdict()
+        for name in well_dict['well']['images']:
+            names.append(name['path'])
+    for pos in range(reader.get_num_positions()):
+        meta = dict()
+        name = names[pos]
+        meta['name'] = name
+        results.append((da.from_zarr(reader.get_zarr(pos)), meta))
+
+    return results
+
+
+def napari_get_reader(path: str):
+    print('here')
+    """Returns a reader for supported paths that include IDR ID.
+    - URL of the form: https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.1/ID.zarr/
+    """
+
+    if isinstance(path, str) and path.endswith(".zarr"):
+        # If we recognize the format, we return the actual reader function
+        return ome_zarr_reader
+
+    # Ignoring this path
+    else:
+        return None
+
+
 def napari_experimental_provide_dock_widget():  # pragma: no-cover
     return [(WorkflowEditorWidget, {"name": "Workflow editor"}), (BatchProcessingWidget, {"name": "Batch processing"})]
